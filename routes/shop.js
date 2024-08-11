540d690890c267f8572e036c1a2bb7a34a821b01
Sakshi_200 <sakshisahu.3475@gmail.com>
2024-08-11 16:40:05 +0530

    html and css files integration

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
-router.use('/', (req, res, next) =>{
-    res.send('<h1>hello</h1>');
+router.get('/', (req, res, next) =>{
+    res.sendFile(path.join( rootDir , 'views', 'shop.html'));
 });
 
 module.exports = router
