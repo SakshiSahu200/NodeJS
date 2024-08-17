40ed609f52674a067838826ab8175b1e435f05f1
Sakshi_200 <sakshisahu.3475@gmail.com>
2024-08-17 23:31:16 +0530

    using pug template and replacing the html files into short code

--- routes/admin.js
+++ routes/admin.js
@@ -5,17 +5,21 @@ const path = require('path');
 
 const rootDir = require('../util/path');
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next)=>{
-    res.sendFile(path.join(rootDir , 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle : 'Add Product'})
+    // res.sendFile(path.join(rootDir , 'views', 'add-product.html'));
     // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
     // res.send('<form action="/admin/add-product" method= "POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next)=>{
-    console.log(req.body);
+    products.push({ title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../util/path');
-
+const adminData = require('./admin');
 const router = express.Router();
+
 router.get('/', (req, res, next) =>{
-    res.sendFile(path.join( rootDir , 'views', 'shop.html'));
+    // console.log(adminData.products);
+    // res.sendFile(path.join( rootDir , 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop'}); // using pug
 });
 
-module.exports = router
+module.exports = router;
