4ce6cacf7847f8a44502fefeaae7b1452d4425bb
Sakshi_200 <sakshisahu.3475@gmail.com>
2024-09-05 09:56:08 +0530

    routing and app enhancement

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error.js')
 const app = express();
 
-app.set('view engine', 'pug'); //to let know express that we are using pug for templates
+// app.set('view engine', 'pug'); //to let know express that we are using pug for templates
+app.set('view engine', 'ejs');
 app.set('views', 'views') // and where we can find it
 
 const adminRoutes = require('./routes/admin');
--- controllers/products.js
+++ controllers/products.js
@@ -1,23 +1,24 @@
-const Product = require('../models/product')
+// const Product = require('../models/product')
 
-exports.getAddProduct = (req, res, next)=>{
-    res.render('add-product', {pageTitle : 'Add Product', path: '/admin/add-product'}); //path can be written anything, its not necessary to write /add-product
-    // res.sendFile(path.join(rootDir , 'views', 'add-product.html'));
-    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-    // res.send('<form action="/admin/add-product" method= "POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-} 
+// exports.getAddProduct = (req, res, next)=>{
+//     res.render('add-product', {pageTitle : 'Add Product', path: '/admin/add-product'}); //path can be written anything, its not necessary to write /add-product
+//     // res.sendFile(path.join(rootDir , 'views', 'add-product.html'));
+//     // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+//     // res.send('<form action="/admin/add-product" method= "POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+// } 
 
-exports.postAddProduct = (req, res, next)=>{
-    // products.push({ title: req.body.title});
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect('/');
-}
+// exports.postAddProduct = (req, res, next)=>{
+//     // products.push({ title: req.body.title});
+//     const product = new Product(req.body.title);
+//     product.save();
+//     res.redirect('/');
+// }
 
-exports.getProducts = (req, res, next) =>{
-    Product.fetchAll((products =>{
-        res.render('shop', {prods: products, pageTitle: 'Shop', path :'/'}); // using pug
-    }));
-    // console.log(adminData.products);
-    // res.sendFile(path.join( rootDir , 'views', 'shop.html'));
-}
+
+// exports.getProducts = (req, res, next) =>{
+//     Product.fetchAll((products =>{
+//         res.render('shop', {prods: products, pageTitle: 'Shop', path :'/'}); // using pug
+//     }));
+//     // console.log(adminData.products);
+//     // res.sendFile(path.join( rootDir , 'views', 'shop.html'));
+// }
